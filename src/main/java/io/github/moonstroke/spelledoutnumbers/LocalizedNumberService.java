9a4aeee228a874f322c70506faee3e90585b60fc
Joachim MARIE <moonstroke+github@live.fr>
2024-12-30 18:01:40 +0100

    Add impl loading method in base service interface
    This method is not intended to be called by users of the interface; it
    is a helper method for subinterfaces. There is unfortunately there is
    no way to declare a protected interface method, so instead an API note
    is added, requesting users not to use the method.

--- src/main/java/io/github/moonstroke/spelledoutnumbers/LocalizedNumberService.java
+++ src/main/java/io/github/moonstroke/spelledoutnumbers/LocalizedNumberService.java
@@ -1,6 +1,7 @@
 package io.github.moonstroke.spelledoutnumbers;
 
 import java.util.Locale;
+import java.util.ServiceLoader;
 
 /**
  * Interface representing any service regarding numeric values that depends on a locale.
@@ -14,4 +15,26 @@ public interface LocalizedNumberService {
 	 * @return this service handler's locale
 	 */
 	Locale getSupportedLocale();
+
+	/**
+	 * Retrieve an implementation of the given service class that supports the specified locale.
+	 * 
+	 * @param <S>          The locale-dependent number service interface
+	 * @param locale       The locale to support
+	 * @param serviceClass The interface for the service to provide
+	 * 
+	 * @return The first discovered matching implementation of the given service interface, or {@code null} if none
+	 *         matches.
+	 *
+	 * @apiNote This method is not to be used directly by users of this interface; it is a helper method for
+	 *          subinterfaces.
+	 */
+	static <S extends LocalizedNumberService> S getHandlerFor(Locale locale, Class<S> serviceClass) {
+		for (S serviceImpl : ServiceLoader.load(serviceClass)) {
+			if (serviceImpl.getSupportedLocale().equals(locale)) {
+				return serviceImpl;
+			}
+		}
+		return null;
+	}
 }
