d17f4bde466250c70578bafbb39d47a747d7dac5
Joachim MARIE <joachim.marie-d-avigneau@csgroup.eu>
2024-12-30 18:09:39 +0100

    Declare service loading methods in subinterfaces
    The two subinterfaces now provide their respective implementation
    loading method. They are, mostly, mere wrappers around the base
    interface's loading method.

--- src/main/java/io/github/moonstroke/spelledoutnumbers/NumberSpeller.java
+++ src/main/java/io/github/moonstroke/spelledoutnumbers/NumberSpeller.java
@@ -1,5 +1,7 @@
 package io.github.moonstroke.spelledoutnumbers;
 
+import java.util.Locale;
+
 /**
  * This interface provides the numeric-to-text translation service.
  *
@@ -14,4 +16,15 @@ public interface NumberSpeller extends LocalizedNumberService {
 	 * @return A textual representation of the given number
 	 */
 	String spellOut(double doubleValue);
+
+	/**
+	 * Find a number speller supporting the specified locale.
+	 *
+	 * @param locale The locale to support
+	 *
+	 * @return a number speller supporting the given locale, or {@code null} if none matched
+	 */
+	static NumberSpeller getNumberSpellerFor(Locale locale) {
+		return LocalizedNumberService.getHandlerFor(locale, NumberSpeller.class);
+	}
 }
