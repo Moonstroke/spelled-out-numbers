b3747a32ce6d689b8636185108b793071a707859
Joachim "Moonstroke" MARIE <jiema@live.fr>
2024-12-31 10:44:46 +0100

    Add a parameterized test method for valid inputs
    The source of the test inputs is also defined, as a mathod returning a
    stream of Arguments, each comprising the string to parse and the
    expected numeric value.
    The source is an explicit array, not a variadic arguments list, because
    an array allows a trailing comma and varargs do not; having this
    trailing comma produces cleaner diffs while the explicit array does not
    hinder code clarity.
    
    The actual test inputs are, for now, the edge cases already implemented
    in the default parser.

--- src/test/java/io/github/moonstroke/spelledoutnumbers/test/DefaultNumericTextParserTest.java
+++ src/test/java/io/github/moonstroke/spelledoutnumbers/test/DefaultNumericTextParserTest.java
@@ -3,9 +3,13 @@ package io.github.moonstroke.spelledoutnumbers.test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Locale;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import io.github.moonstroke.spelledoutnumbers.NumericTextParser;
 
@@ -26,5 +30,22 @@ class DefaultNumericTextParserTest {
 	@Test
 	void testParserCannotParseEmptyString() {
 		assertThrows(NumberFormatException.class, () -> parser.parse(""));
+	}
+
+	@ParameterizedTest
+	@MethodSource("validInputs")
+	void testParserParsesValidValues(String input, double expected) {
+		assertEquals(expected, parser.parse(input));
+	}
+
+	static Stream<Arguments> validInputs() {
+		/* Explicit array allows trailing comma for cleaner diffs */
+		return Stream.of(new Arguments[] {
+				Arguments.of("not a number", Double.NaN),
+				Arguments.of("infinity", Double.POSITIVE_INFINITY),
+				Arguments.of("minus infinity", Double.NEGATIVE_INFINITY),
+				Arguments.of("zero", 0.),
+				Arguments.of("minus zero", -0.),
+		});
 	}
 }
