70a366392db072387e39f26865432cac2fbefef3
Joachim MARIE <moonstroke+github@live.fr>
2024-12-31 10:57:36 +0100

    Add a parameterized speller test method
    This time also the parameter source is a method returning a stream of
    Arguments containing the edge cases already implemented in the default
    speller.

--- src/test/java/io/github/moonstroke/spelledoutnumbers/test/DefaultNumberSpellerTest.java
+++ src/test/java/io/github/moonstroke/spelledoutnumbers/test/DefaultNumberSpellerTest.java
@@ -3,9 +3,12 @@ package io.github.moonstroke.spelledoutnumbers.test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Locale;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import io.github.moonstroke.spelledoutnumbers.NumberSpeller;
 
@@ -18,8 +21,20 @@ class DefaultNumberSpellerTest {
 		speller = NumberSpeller.getNumberSpellerFor(Locale.getDefault());
 	}
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	@ParameterizedTest
+	@MethodSource("validInputs")
+	void testSpellerSpellsValidInputs(double input, String expected) {
+		assertEquals(expected, speller.spellOut(input));
+	}
+
+	static Stream<Arguments> validInputs() {
+		/* Explicit array allows trailing comma for cleaner diffs */
+		return Stream.of(new Arguments[] {
+				Arguments.of(Double.NaN, "not a number"),
+				Arguments.of(Double.POSITIVE_INFINITY, "infinity"),
+				Arguments.of(Double.NEGATIVE_INFINITY, "minus infinity"),
+				Arguments.of(0., "zero"),
+				Arguments.of(-0., "zero"),
+		});
 	}
 }
