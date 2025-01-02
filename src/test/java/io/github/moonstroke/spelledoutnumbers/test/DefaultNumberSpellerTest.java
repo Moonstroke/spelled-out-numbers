fdad6760c8745fd13b571ae0579cad9f4b56abde
Joachim MARIE <moonstroke+github@live.fr>
2025-01-02 01:00:41 +0100

    Let JUnit take care of the file loading
    The test input source is changed from method to CSV file in the
    annotation to the test method. This allows to remove entirely the CSV
    loading method.
    However this change was not without impact: for it to work properly, the
    order of the arguments to the parser test method has to be changed to
    match the CSV value order.

--- src/test/java/io/github/moonstroke/spelledoutnumbers/test/DefaultNumberSpellerTest.java
+++ src/test/java/io/github/moonstroke/spelledoutnumbers/test/DefaultNumberSpellerTest.java
@@ -2,17 +2,10 @@ package io.github.moonstroke.spelledoutnumbers.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.Locale;
-import java.util.stream.Collectors;
-
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import io.github.moonstroke.spelledoutnumbers.NumberSpeller;
 
@@ -26,19 +19,8 @@ class DefaultNumberSpellerTest {
 	}
 
 	@ParameterizedTest
-	@MethodSource("validInputs")
+	@CsvFileSource(resources = {"/numbers.csv"}, numLinesToSkip = 1)
 	void testSpellerSpellsValidInputs(double input, String expected) {
 		assertEquals(expected, speller.spellOut(input));
-	}
-
-	static Iterable<Arguments> validInputs() throws IOException {
-		try (InputStream is = DefaultNumberSpellerTest.class.getResourceAsStream("/numbers.csv");
-		     BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
-			return br.lines()
-			         .skip(1) /* Header line */
-			         .map(line -> line.split(",", 2))
-			         .map(fields -> Arguments.of(Double.parseDouble(fields[0]), fields[1]))
-			         .collect(Collectors.toList());
-		}
 	}
 }
