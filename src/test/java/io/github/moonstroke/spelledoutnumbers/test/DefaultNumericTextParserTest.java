aab7efa52e0ea40455b00ad674c62c782bc8677d
Moonstroke <jiema@live.fr>
2025-01-02 01:00:41 +0100

    Let JUnit take care of the file loading
    The test input source is changed from method to CSV file in the
    annotation to the test method. This allows to remove entirely the CSV
    loading method.
    However this change was not without impact: for it to work properly, the
    order of the arguments to the parser test method has to be changed to
    match the CSV value order.

--- src/test/java/io/github/moonstroke/spelledoutnumbers/test/DefaultNumericTextParserTest.java
+++ src/test/java/io/github/moonstroke/spelledoutnumbers/test/DefaultNumericTextParserTest.java
@@ -2,19 +2,11 @@ package io.github.moonstroke.spelledoutnumbers.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.Locale;
-import java.util.stream.Collectors;
-
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-
+import org.junit.jupiter.params.provider.CsvFileSource;
 import io.github.moonstroke.spelledoutnumbers.NumericTextParser;
 
 class DefaultNumericTextParserTest {
@@ -37,19 +29,8 @@ class DefaultNumericTextParserTest {
 	}
 
 	@ParameterizedTest
-	@MethodSource("validInputs")
-	void testParserParsesValidValues(String input, double expected) {
+	@CsvFileSource(resources = {"/numbers.csv"}, numLinesToSkip = 1)
+	void testParserParsesValidValues(double expected, String input) {
 		assertEquals(expected, parser.parse(input));
-	}
-
-	static Iterable<Arguments> validInputs() throws IOException {
-		try (InputStream is = DefaultNumberSpellerTest.class.getResourceAsStream("/numbers.csv");
-		     BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
-			return br.lines()
-			         .skip(1) /* Header line */
-			         .map(line -> line.split(",", 2))
-			         .map(fields -> Arguments.of(fields[1], Double.parseDouble(fields[0])))
-			         .collect(Collectors.toList());
-		}
 	}
 }
