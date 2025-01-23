64007d249c8ffef6fd15e2b7d6745cf89244731c
Moonstroke <jiema@live.fr>
2025-01-23 15:48:45 +0100

    Document the spelled-out number class
    The class and its constructor are given fully written Javadoc block
    comments; the overridden Number methods only explicitly inherit the
    base class' documentation block.

--- src/main/java/io/github/moonstroke/spelledoutnumbers/SpelledOutNumber.java
+++ src/main/java/io/github/moonstroke/spelledoutnumbers/SpelledOutNumber.java
@@ -2,6 +2,11 @@ package io.github.moonstroke.spelledoutnumbers;
 
 import java.util.Locale;
 
+/**
+ * A (real) number, with its textual representation in a given locale.
+ *
+ * @author Moonstroke
+ */
 public class SpelledOutNumber extends Number {
 	private static final long serialVersionUID = -2400427537564695903L;
 
@@ -10,27 +15,45 @@ public class SpelledOutNumber extends Number {
 	private final String transcription;
 
 
+	/**
+	 * Construct a spelled-out number from its numeric value for the specified locale.
+	 *
+	 * @param locale      The locale in which to transcribe the number
+	 * @param doubleValue The real numeric value
+	 */
 	public SpelledOutNumber(Locale locale, double doubleValue) {
 		this.locale = locale;
 		this.transcription = null; // TODO
 		this.doubleValue = doubleValue;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public int intValue() {
 		return (int) doubleValue;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public long longValue() {
 		return (long) doubleValue;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public float floatValue() {
 		return (float) doubleValue;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public double doubleValue() {
 		return doubleValue;
