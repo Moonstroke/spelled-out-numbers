fce777835111357dd7a3646078ef93caffc53089
Joachim MARIE <moonstroke+github@live.fr>
2025-01-27 17:32:44 +0100

    Set mode and time unit for the benchmark class
    ... to "average time" and microseconds, respectively.

--- benchmarks/src/main/java/io/github/moonstroke/spelledoutnumbers/benchmarks/UsEnglishNumberSpellerBenchmark.java
+++ benchmarks/src/main/java/io/github/moonstroke/spelledoutnumbers/benchmarks/UsEnglishNumberSpellerBenchmark.java
@@ -1,8 +1,12 @@
 package io.github.moonstroke.spelledoutnumbers.benchmarks;
 
 import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.infra.Blackhole;
@@ -10,7 +14,9 @@ import org.openjdk.jmh.infra.Blackhole;
 import io.github.moonstroke.spelledoutnumbers.NumberSpeller;
 
 
+@BenchmarkMode(Mode.AverageTime)
 @State(Scope.Thread)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
 public class UsEnglishNumberSpellerBenchmark {
 	final NumberSpeller speller = NumberSpeller.getNumberSpellerFor(Locale.US);
 
